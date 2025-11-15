5a44fb56368914f9285592485b743cced26e39cb
Namrata <nammu@kjattimbp.local>
2025-11-15 11:06:21 -0500

    try diff init for cola a n b with scaled 0.4

--- CoLA/scripts/cola_scripts/cola60m_all_old.sh
+++ CoLA/scripts/cola_scripts/cola60m_all_old.sh
@@ -179,7 +179,7 @@ CUDA_VISIBLE_DEVICES=7 torchrun --standalone --nproc-per-node=1 main.py \
 
 
 # Cola + WSD + hp tuning
-CUDA_VISIBLE_DEVICES=4 torchrun --standalone --nproc-per-node=1 main_withwandb.py \
+CUDA_VISIBLE_DEVICES=0 torchrun --standalone --nproc-per-node=1 main_withwandb.py \
     --model_type cola \
     --model_config cola_configs/cola_60m.json \
     --lr 0.006 \
